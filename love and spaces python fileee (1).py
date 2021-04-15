b998e81fa894a0931e03d14fa7f0cbb56056a32f
Trajon Brown <80763199+Trajonb@users.noreply.github.com>
2021-04-15 10:02:02 -0400

    Add files via upload

---
+++ love and spaces python fileee (1).py
@@ -0,0 +1,23 @@
+Python 3.4.2 (v3.4.2:ab2c023a9432, Oct  5 2014, 20:42:22) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> ================================ RESTART ================================
+>>> 
+If I speak in the tongues of men or of angels, but do not have LOVE, I am only a resounding gong or a clanging cymbal.
+If I have the gift of prophecy and can fathom all mysteries and all knowledge, and if I have a faith that can move mountains, but do not have LOVE, I am nothing.
+If I give all I possess to the poor and give over my body to hardship that I may boast, but do not have LOVE, I gain nothing.
+LOVE is patient, LOVE is kind.
+It does not envy, it does not boast, it is not proud.
+It does not dishonor others, it is not self-seeking, it is not easily angered, it keeps no record of wrongs.
+LOVE does not delight in evil but rejoices with the truth.
+It always protects, always trusts, always hopes, always perseveres.
+LOVE never fails.
+But where there are prophecies, they will cease; where there are tongues, they will be stilled; where there is knowledge, it will pass away.
+For we know in part and we prophesy in part, but when completeness comes, what is in part disappears.
+When I was a child, I talked like a child, I thought like a child, I reasoned like a child.
+When I became a man, I put the ways of childhood behind me.
+For now we see only a reflection as in a mirror; then we shall see face to face.
+Now I know in part; then I shall know fully, even as I am fully known.
+And now these three remain: faith, hope and LOVE.
+But the greatest of these is LOVE.
+>>> 
